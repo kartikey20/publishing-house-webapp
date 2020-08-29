1cc2f0246bbf6ee5808e6a038b9fb96eca1e24b9
Kartikey <ranjankartikey1999@gmail.com>
2020-08-29 18:40:46 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import NavigationBar from "./Components/Navigation";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Author from "./Pages/Author";
+import AuthorPage from "./Pages/AuthorPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <><Router>
+            <NavigationBar/>
+            <Switch>
+                <Route path='/' exact component={Home}/>
+                <Route path={'/About'} component={About}/>
+                <Route path={'/Author'} component={Author}/>
+                <Route path={'/Authors/:name'} component={AuthorPage}/>
+            </Switch>
+        </Router>
+        </>
+    );
 }
 
 export default App;
