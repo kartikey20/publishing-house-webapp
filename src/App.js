51b096b108dc842b363f9a17361f6ee710fff8b2
Kartikey <ranjankartikey1999@gmail.com>
2020-08-30 10:07:42 +0530

    Added particle background

--- src/App.js
+++ src/App.js
@@ -10,18 +10,25 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Author from "./Pages/Author";
 import AuthorPage from "./Pages/AuthorPage";
+import Contact from "./Pages/Contact";
+import ParticlesBg from "particles-bg";
 
 function App() {
     return (
-        <><Router>
-            <NavigationBar/>
-            <Switch>
-                <Route path='/' exact component={Home}/>
-                <Route path={'/About'} component={About}/>
-                <Route path={'/Author'} component={Author}/>
-                <Route path={'/Authors/:name'} component={AuthorPage}/>
-            </Switch>
-        </Router>
+        <>
+            <Router>
+
+                <NavigationBar/>
+
+                <Switch>
+                    <Route path='/' exact component={Home}/>
+                    <Route path={'/About'} component={About}/>
+                    <Route path={'/Author'} component={Author}/>
+                    <Route path={'/Authors/:name'} component={AuthorPage}/>
+                    <Route path={'/Contact'} component={Contact}/>
+                </Switch>
+            </Router>
+            <ParticlesBg type="cobweb" color="#a6a4ad" num={70} bg={true} />
         </>
     );
 }
