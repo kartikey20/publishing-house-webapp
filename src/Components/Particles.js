3ef63111a060ad99b5a9358433a26cd40427ba96
Kartikey <ranjankartikey1999@gmail.com>
2020-08-30 10:13:04 +0530

    added Particles.js

---
+++ src/Components/Particles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Particles } from '@blackbox-vision/react-particles';
+
+const ParticlesJs = () => (
+    <Particles
+        id="simple"
+        width="auto"
+        height="100vh"
+        style={{
+            backgroundColor: 'blue',
+        }}
+        params={{
+            particles: {
+                number: {
+                    value: 50,
+                },
+                size: {
+                    value: 3,
+                },
+            },
+            interactivity: {
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: 'repulse',
+                    },
+                },
+            },
+        }}
+    />
+);
+
+export default ParticlesJs
